1872ac7b0e5b75991b6c3c84b31abe210802f565
zaloAustine <austinezaloa@gmail.com>
2024-06-08 22:28:09 +0300

    converted code to use mvi

---
+++ app/src/test/java/com/zalo/coders/composepractice/ui/calculator/CalculatorViewModelTest.kt
@@ -0,0 +1,54 @@
+package com.zalo.coders.composepractice.ui.calculator
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+/**
+Created by zaloaustine in 6/8/24.
+ */
+
+class CalculatorViewModelTest {
+
+    private val viewModel = CalculatorViewModel()
+
+
+    @Test
+    fun testInitialState() {
+        assertEquals("", viewModel.state.operation)
+        assertEquals("", viewModel.state.result)
+    }
+
+    @Test
+    fun testNumberClicked() {
+        viewModel.onAction(CalculatorAction.NumberClicked("1"))
+        viewModel.onAction(CalculatorAction.NumberClicked("+"))
+        viewModel.onAction(CalculatorAction.NumberClicked("1"))
+        assertEquals("1+1", viewModel.state.operation)
+    }
+
+    @Test
+    fun testCalculate() {
+        viewModel.onAction(CalculatorAction.NumberClicked("1"))
+        viewModel.onAction(CalculatorAction.NumberClicked("+"))
+        viewModel.onAction(CalculatorAction.NumberClicked("1"))
+        assertEquals("2", viewModel.state.result)
+    }
+
+    @Test
+    fun testDelete() {
+        viewModel.onAction(CalculatorAction.NumberClicked("1"))
+        viewModel.onAction(CalculatorAction.NumberClicked("+"))
+        viewModel.onAction(CalculatorAction.NumberClicked("1"))
+        viewModel.onAction(CalculatorAction.Delete(isClear = true))
+        assertEquals("1+", viewModel.state.operation)
+    }
+
+    @Test
+    fun testDeleteAll() {
+        viewModel.onAction(CalculatorAction.NumberClicked("1"))
+        viewModel.onAction(CalculatorAction.NumberClicked("2"))
+        viewModel.onAction(CalculatorAction.NumberClicked("3"))
+
+        viewModel.onAction(CalculatorAction.DeleteAll)
+        assertEquals("", viewModel.state.operation)
+    }
+}
