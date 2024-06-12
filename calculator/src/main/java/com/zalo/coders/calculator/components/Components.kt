8e59a2e967904f4fdba3f55cbfef79d85546fddd
zaloAustine <austinezaloa@gmail.com>
2024-06-12 15:39:39 +0300

    fix bug ...started work on lanscape

--- calculator/src/main/java/com/zalo/coders/calculator/components/Components.kt
+++ calculator/src/main/java/com/zalo/coders/calculator/components/Components.kt
@@ -1,5 +1,6 @@
 package com.zalo.coders.calculator.components
 
+import android.content.res.Configuration
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
@@ -9,7 +10,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,8 +18,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridItemSpanScope
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
@@ -28,16 +32,21 @@ import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zalo.coders.calculator.R
@@ -87,7 +96,7 @@ fun AnswerView(operation: String, result: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(24.dp),
+            .padding(end = 24.dp),
         horizontalAlignment = Alignment.End,
         verticalArrangement = Arrangement.Bottom
     ) {
@@ -147,13 +156,11 @@ fun NumberPad(
                         deleteAll.invoke()
                     }
 
-                    "%", "*", "/", "-", "+" -> {
+                    "%", "*", "/", "-", "+", "." -> {
                         if (currentOperation.isEmpty() || !currentOperation.last().isDigit()) {
                             numberClicked.invoke("")
                         } else {
-                            if (isValidInput(valueClicked)) {
-                                numberClicked.invoke(valueClicked)
-                            }
+                            numberClicked.invoke(valueClicked)
                         }
                     }
 
@@ -173,9 +180,23 @@ fun NumberPad(
 
 @Composable
 fun CircleWithText(text: String, modifier: Modifier = Modifier, onClick: (String) -> Unit) {
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+
+    val size by remember(configuration) {
+        derivedStateOf {
+            if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                (screenHeight.times(0.2f)).coerceAtMost(screenWidth.times(0.2f))
+            } else {
+                (screenHeight * 0.2f).coerceAtMost(screenWidth * 0.2f)
+            }
+        }
+    }
+
     Box(
         modifier = modifier
-            .size(95.dp)
+            .size(size)
             .clickable { onClick.invoke(text) }
             .padding(8.dp)
             .background(color = getColor(text), shape = CircleShape)
@@ -201,6 +222,68 @@ fun CircleWithText(text: String, modifier: Modifier = Modifier, onClick: (String
                 fontFamily = FontFamily.Monospace,
                 textAlign = TextAlign.Start
             )
+        }
+    }
+}
+
+
+@Composable
+fun NumberPadLandscape(
+    modifier: Modifier = Modifier,
+    currentOperation: String,
+    numberClicked: (String) -> Unit,
+    calculate: () -> Unit,
+    delete: () -> Unit,
+    deleteAll: () -> Unit,
+) {
+    Box {
+        LazyHorizontalGrid(
+            rows = GridCells.Fixed(4),
+            modifier = modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            items(padItems.size, span = {
+                if (padItems[it] == "0") {
+                    GridItemSpan(2)
+                } else {
+                    GridItemSpan(1)
+                }
+            }) {
+                val text = padItems[it]
+                CircleWithText(text = padItems[it]) { valueClicked ->
+                    when (text) {
+                        "=" -> {
+                            calculate.invoke()
+                        }
+
+                        "C" -> {
+                            delete.invoke()
+                        }
+
+                        "AC" -> {
+                            deleteAll.invoke()
+                        }
+
+                        "%", "*", "/", "-", "+", "." -> {
+                            if (currentOperation.isEmpty() || !currentOperation.last().isDigit()) {
+                                numberClicked.invoke("")
+                            } else {
+                                numberClicked.invoke(valueClicked)
+                            }
+                        }
+
+                        else -> {
+                            if (isValidInput(valueClicked)) {
+                                numberClicked.invoke(valueClicked)
+                            } else {
+                                numberClicked.invoke("")
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
