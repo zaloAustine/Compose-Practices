ecf257532740dba6febd6e69ccd11695a12dded8
zaloAustine <austinezaloa@gmail.com>
2024-06-12 15:40:20 +0300

    fix bug ...started work on lanscape

--- calculator/src/main/java/com/zalo/coders/calculator/components/Components.kt
+++ calculator/src/main/java/com/zalo/coders/calculator/components/Components.kt
@@ -4,7 +4,6 @@ import android.content.res.Configuration
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,44 +11,32 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyGridItemSpanScope
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.zalo.coders.calculator.R
 import com.zalo.coders.calculator.core.getColor
 import com.zalo.coders.calculator.core.isValidInput
 import com.zalo.coders.calculator.core.padItems
@@ -64,9 +51,7 @@ fun DarkModeSelectorView(
     modifier: Modifier = Modifier,
     onIconClick: (Boolean) -> Unit
 ) {
-    val icon = if (darkMode) painterResource(R.drawable.sun) else painterResource(R.drawable.moon)
     val iconColor = remember { Animatable(Color.Gray) }
-
     LaunchedEffect(darkMode) {
         iconColor.animateTo(
             targetValue = if (darkMode) Color.Yellow else Color.Gray,
